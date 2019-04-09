96c33fd25151292f4577d263583b39a640e64e9b
JungHoon Park <wfr112@naver.com>
2019-04-09 15:17:58 +0900

    room socket 업데이트

--- src/socketManager.js
+++ src/socketManager.js
@@ -3,11 +3,34 @@ const socketConfing = require('./config/socket')
 module.exports = (server) => {
   const io = require('socket.io')(server, socketConfing)
 
-  io.on('connection', socket => {
+  // TODO: 코드 분리
+  // TODO: 원인 모르는 끊기는 문제 해결
+
+  const room = io.of('/room')
+  const mainPage = 'mainpage'
+
+  room.on('connection', socket => {
+    socket.on('join', () => {
+      socket.join(mainPage)
+      console.log(`${mainPage}에 조인함`)
+    })
+
+    /**
+     * @desc: 방을 만들 시 mainPage에 접속해있는 유저에게 방 만들었다고 알려줌
+     *  */ 
     socket.on('create', data => {
-      console.log(data)
-      socket.join('room')
-      io.to('room').emit('create', data.roomId)
+      room.to(mainPage).emit('message', data.roomId)
+      console.log(JSON.stringify(data) + "방 생성")
+    })
+
+    socket.on('leave', () => {
+      socket.leave(mainPage)
+      console.log(`${mainPage} 에서 떠남`)
+    })
+
+    socket.on('disconnect', () => {
+      socket.leave(mainPage)
+      console.log(`${mainPage} 디스커넥트`)
     })
   })
 }

47100fefbb926ac8a2c18d02a7156171980ad4bf
JungHoon Park <wfr112@naver.com>
2019-04-23 14:22:34 +0900

    bug fix: 소켓 중복생성 수정

--- src/socketio/routes/channals.js
+++ src/socketio/routes/channals.js
@@ -1,45 +1,55 @@
 module.exports = (io, roomId) => {
   const room = io.of(`/${roomId}`)
-  const novelPage = roomId
+  const pageName = roomId
   let sockets = []
 
   room.on('connection', socket => {
-    sockets.push(socket)
-    console.log(`socket: ${socket.id} 생성됨`)
+    room.clients((err, clients) => {
+      if (err) throw err
+      console.log(clients)
+    })
+
     console.log('현재 소켓의 길이', sockets.length)
 
-    socket.on('join', () => {
-      socket.join(novelPage)
-      console.log(`${novelPage}에 조인함`)
+    socket.on('join', data => {
+      socket.join(pageName)
+
+      console.log(`${pageName}에 조인함`, data)
     })
 
     // @desc: 글 쓸시 접속해있는 유저에게 글 뿌려줌
-    // @params: data: {roomId: number, msg: string}
+    // @params: data: {roomId: string, msg: string}
     socket.on('create', data => {
-      room.to(novelPage).emit('message', data)
+      room.to(pageName).emit('message', data)
+
       console.log(JSON.stringify(data) + '글 씀')
     })
 
     socket.on('leave', () => {
       sockets.splice(sockets.indexOf(socket), 1)
-      socket.leave(novelPage)
-      console.log(`${novelPage} 에서 떠남`)
+      socket.leave(pageName)
+
+      console.log(`${pageName} 에서 떠남`)
     })
 
     socket.on('disconnect', () => {
       sockets.splice(sockets.indexOf(socket), 1)
-      socket.leave(novelPage)
-      console.log(`${novelPage} 디스커넥트`)
+      socket.disconnect(true)
+
+      console.log(`${pageName} 디스커넥트`)
     })
 
     socket.on('error', err => {
+      socket.disconnect(true)
+
       console.log('에러 발생:', err)
     })
 
     // @desc 방 글쓰기 참여
-    // @params: data: {roomId: number}
+    // @params: data: {roomId: string, userId: number}
     socket.on('joinToWrite', data => {
-      room.to(novelPage).emit('message', data)
+      room.to(pageName).emit('message', data)
+
       console.log(JSON.stringify(data) + '방에 참가함')
     })
   })
--- src/socketio/routes/mainpage.js
+++ src/socketio/routes/mainpage.js
@@ -2,22 +2,33 @@ module.exports = io => {
   const room = io.of('/mainpage')
   const mainPage = 'mainpage'
   let sockets = []
-
+  let rooms = new Set()
   room.on('connection', socket => {
     sockets.push(socket)
-    console.log(`socket: ${socket.id} 생성됨`)
-    console.log('현재 소켓의 길이', sockets.length)
+
+    console.log(mainPage + ' 에 접속중 :', sockets.length)
 
     socket.on('join', () => {
       socket.join(mainPage)
-      console.log(`${mainPage}에 조인함`)
+
+      console.log(`${mainPage} 에 조인함`)
     })
 
     // @desc: 방을 만들 시 mainPage에 접속해있는 유저에게 방 만들었다고 알려줌
     socket.on('create', data => {
       room.to(mainPage).emit('message', data.roomId)
       require('./channals')(io, data.roomId)
-      console.log(JSON.stringify(data) + '방 생성')
+
+      console.log(mainPage + ' 에서 방 생성' + JSON.stringify(data))
+    })
+
+    socket.on('joinChannal', data => {
+      if (!rooms.has(data.roomId)) {
+        rooms.add(data.roomId)
+        require('./channals')(io, data.roomId)
+
+        console.log(`${mainPage} 에서 ${data.roomId} 에 접속함`)
+      }
     })
 
     socket.on('leave', () => {
@@ -35,7 +46,7 @@ module.exports = io => {
     })
 
     socket.on('error', err => {
-      console.log('에러 발생:', err)
+      console.log(`${mainPage} 에러발생`, err)
     })
   })
 }
